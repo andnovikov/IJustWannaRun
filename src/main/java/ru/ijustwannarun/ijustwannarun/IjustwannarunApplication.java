5eaee0c676118a3385cc7a9d480df7a9a8a67664
anovikov <andnovikov@outlook.com>
2020-01-01 19:16:28 +0300

    Spring Initializr

---
+++ src/main/java/ru/ijustwannarun/ijustwannarun/IjustwannarunApplication.java
@@ -0,0 +1,13 @@
+package ru.ijustwannarun.ijustwannarun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjustwannarunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjustwannarunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ijustwannarun/ijustwannarun/IjustwannarunApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ijustwannarun.ijustwannarun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjustwannarunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
